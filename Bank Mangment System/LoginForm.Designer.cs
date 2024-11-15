0b7c62e7a2edcbf8f36dda512c4318bc56572f1b
Ahmed-Mohaned <ahmedmohunedsabah@gmail.com>
2024-11-16 02:17:34 +0300

    Removed unnecessary files

--- Bank Mangment System/LoginForm.Designer.cs
+++ Bank Mangment System/LoginForm.Designer.cs
@@ -29,65 +29,164 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Email_Logintxt = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.PictureBox();
+            this.Password_Logintxt = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.password)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(312, 650);
-            this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(-21, -4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(280, 130);
+            this.pictureBox1.Size = new System.Drawing.Size(579, 564);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(621, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 136);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(12, 165);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(555, 145);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Get started from Login Below";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(555, 185);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(322, 109);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Email_Logintxt
+            // 
+            this.Email_Logintxt.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Email_Logintxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Email_Logintxt.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email_Logintxt.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Email_Logintxt.Location = new System.Drawing.Point(622, 218);
+            this.Email_Logintxt.Multiline = true;
+            this.Email_Logintxt.Name = "Email_Logintxt";
+            this.Email_Logintxt.Size = new System.Drawing.Size(210, 44);
+            this.Email_Logintxt.TabIndex = 4;
+            this.Email_Logintxt.Text = "E-mail";
+            this.Email_Logintxt.TextChanged += new System.EventHandler(this.Email_Logintxt_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Image = ((System.Drawing.Image)(resources.GetObject("password.Image")));
+            this.password.Location = new System.Drawing.Point(554, 284);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(322, 109);
+            this.password.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.password.TabIndex = 5;
+            this.password.TabStop = false;
+            // 
+            // Password_Logintxt
+            // 
+            this.Password_Logintxt.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Password_Logintxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Password_Logintxt.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password_Logintxt.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Password_Logintxt.Location = new System.Drawing.Point(622, 315);
+            this.Password_Logintxt.Multiline = true;
+            this.Password_Logintxt.Name = "Password_Logintxt";
+            this.Password_Logintxt.Size = new System.Drawing.Size(210, 44);
+            this.Password_Logintxt.TabIndex = 6;
+            this.Password_Logintxt.Text = "Password";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(533, 384);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(339, 109);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(140)))), ((int)(((byte)(40)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(587, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 51);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(49)))), ((int)(((byte)(66)))));
-            this.ClientSize = new System.Drawing.Size(1000, 650);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(884, 557);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Password_Logintxt);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.Email_Logintxt);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "LoginForm";
             this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.password)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox Email_Logintxt;
+        private System.Windows.Forms.PictureBox password;
+        private System.Windows.Forms.TextBox Password_Logintxt;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Bank Mangment System/LoginForm.cs
+++ Bank Mangment System/LoginForm.cs
@@ -17,13 +17,68 @@ namespace Bank_Mangment_System
             InitializeComponent();
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It work");
+        }
+
         private void LoginForm_Load(object sender, EventArgs e)
         {
-            label1.Text = "";
-            label1.Text = "Astrabank is your trusted financial\r\n" +
-                          "partner, offering secure and innovative\r\n" +
-                          " banking solutions to help " +
-                          "\r\nyou achieve your financial goal";
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0; // إلغاء الحدود
+            button1.FlatAppearance.MouseDownBackColor = Color.DarkOrange; // لون عند الضغط
+            button1.FlatAppearance.MouseOverBackColor = Color.DarkOrange; // لون عند المرور
+
+            Email_Logintxt.Text = "Enter your email";
+            Email_Logintxt.ForeColor = SystemColors.ControlDark;
+
+            Password_Logintxt.Text = "Password";
+            Password_Logintxt.ForeColor = SystemColors.ControlDark;
+
+            // ربط الأحداث
+            Email_Logintxt.Enter += textBox_Enter;
+            Email_Logintxt.Leave += textBox_Leave;
+
+            Password_Logintxt.Enter += textBox_Enter;
+            Password_Logintxt.Leave += textBox_Leave;
+        }
+        private void textBox_Enter(object sender, EventArgs e)//When click on textbox
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox.Text == "Enter your email" || textBox.Text == "Password")
+            {
+                textBox.Text = ""; // مسح النص الافتراضي
+                textBox.ForeColor = Color.Black; // تغيير لون النص إلى الأسود
+            }
+        }
+        private void textBox_Leave(object sender, EventArgs e)//When leave on textbox
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox.Text == "")
+            {
+                // إعادة النص الافتراضي عند الخروج
+                if (textBox.Name == "Email_Logintxt")
+                {
+                    textBox.Text = "Enter your email";
+                }
+                else if (textBox.Name == "Password_Logintxt")
+                {
+                    textBox.Text = "Password";
+                }
+
+                textBox.ForeColor = SystemColors.ControlDark; // تغيير لون النص إلى لون توضيحي
+            }
+        }
+        private void Email_Logintxt_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
