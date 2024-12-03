4bbcaa6f5b0577765bc310a48bbf197d1838e2e8
Ahmed-Mohaned <ahmedmohunedsabah@gmail.com>
2024-12-03 03:55:33 +0300

    1

--- Bank Mangment System/MainForm.cs
+++ Bank Mangment System/MainForm.cs
@@ -126,42 +126,42 @@ namespace Bank_Mangment_System
                     NamesdataGridView.Columns.Add("Last name", "Last name");
                 }
 
-                string searchName = Searchtxtbox.Text;
+                string searchName = Searchtxtbox.Text.Trim(); // استخدام Trim() لإزالة المسافات الزائدة
                 using (StreamReader sr = new StreamReader(ClientFilePath))
                 {
                     string line;
                     NamesdataGridView.Rows.Clear(); // تفريغ الصفوف السابقة
                     while ((line = sr.ReadLine()) != null)
                     {
-                        // تقسيم البيانات المحفوظة (التي تم تخزينها باستخدام الفواصل)
                         string[] userData = line.Split(';');
-                        FullName = userData[0]+" "+userData[1]+" "+userData[2]+" "+userData[3];
-                        FullMotherName = userData[4]+" "+ userData[5];
-                        PhoneNumber = userData[6];
-                        Age = userData[7];
-                        Gender = userData[8];
-                        Location = "District: "+userData[9]+"/"+ "Alley: " + userData[10]+"/"+ "House: " + userData[11];
-                        Email = userData[12];
-                        Password = userData[13];
-                        NationalIDCardPath = userData[14];
-                        PassportPath = userData[15];
-                        ResidenceCardPath = userData[16];
-                        Balance = Convert.ToDecimal(userData[17]);
-                        CardNumber = userData[18];
                         string FirstName = userData[0];
                         string LastName = userData[1];
-                        if (searchName == FirstName)
+
+                        // البحث في الاسم الأول والأخير
+                        if (FirstName.Contains(searchName) || LastName.Contains(searchName))
+                        {
+                            FullName = FirstName + " " + LastName + " " + userData[2] + " " + userData[3];
+                            FullMotherName = userData[4] + " " + userData[5];
+                            PhoneNumber = userData[6];
+                            Age = userData[7];
+                            Gender = userData[8];
+                            Location = "District: " + userData[9] + "/" + "Alley: " + userData[10] + "/" + "House: " + userData[11];
+                            Email = userData[12];
+                            Password = userData[13];
+                            NationalIDCardPath = userData[14];
+                            PassportPath = userData[15];
+                            ResidenceCardPath = userData[16];
+                            Balance = Convert.ToDecimal(userData[17]);
+                            CardNumber = userData[18];
+
                             NamesdataGridView.Rows.Add(FirstName, LastName);
-
-
-
-
-
-
-
+                        }
                     }
+                }
 
-
+                if (NamesdataGridView.Rows.Count == 0)
+                {
+                    MessageBox.Show("No matching records found.", "Search Results");
                 }
             }
             catch (Exception ex)
@@ -170,22 +170,81 @@ namespace Bank_Mangment_System
             }
         }
 
-        
 
-       private void NamesdataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+
+        private void NamesdataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex >= 0)
             {
-                string fullName = NamesdataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
-                OpenClientDetailForm(fullName);
+                string firstName = NamesdataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+                string lastName = NamesdataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+                // البحث عن المستخدم المختار لفتح تفاصيله
+                using (StreamReader sr = new StreamReader(ClientFilePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] userData = line.Split(';');
+                        if (userData[0] == firstName && userData[1] == lastName)
+                        {
+                            FullName = userData[0] + " " + userData[1] + " " + userData[2] + " " + userData[3];
+                            FullMotherName = userData[4] + " " + userData[5];
+                            PhoneNumber = userData[6];
+                            Age = userData[7];
+                            Gender = userData[8];
+                            Location = "District: " + userData[9] + "/" + "Alley: " + userData[10] + "/" + "House: " + userData[11];
+                            Email = userData[12];
+                            Password = userData[13];
+                            NationalIDCardPath = userData[14];
+                            PassportPath = userData[15];
+                            ResidenceCardPath = userData[16];
+                            Balance = Convert.ToDecimal(userData[17]);
+                            CardNumber = userData[18];
+
+                            OpenClientDetailForm(FullName);
+                            break;
+                        }
+                    }
+                }
             }
         }
 
         private void OpenClientDetailForm(string fullName)
         {
-            var clientDetailForm = new ClientDetailForm(FullName, FullMotherName, PhoneNumber
-                                                         , Age, Gender,Location,Email,Password,NationalIDCardPath,PassportPath,ResidenceCardPath,Balance,CardNumber,ClientFilePath);
-            clientDetailForm.Show();
+            // البحث عن بيانات الشخص المحدد في الملف
+            using (StreamReader sr = new StreamReader(ClientFilePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] userData = line.Split(';');
+                    string currentFullName = userData[0] + " " + userData[1] + " " + userData[2] + " " + userData[3];
+
+                    if (currentFullName == fullName)
+                    {
+                        // تمرير البيانات الصحيحة للشخص المحدد
+                        var clientDetailForm = new ClientDetailForm(
+                            currentFullName,                     // FullName
+                            userData[4] + " " + userData[5],     // FullMotherName
+                            userData[6],                         // PhoneNumber
+                            userData[7],                         // Age
+                            userData[8],                         // Gender
+                            "District: " + userData[9] + "/" + "Alley: " + userData[10] + "/" + "House: " + userData[11], // Location
+                            userData[12],                        // Email
+                            userData[13],                        // Password
+                            userData[14],                        // NationalIDCardPath
+                            userData[15],                        // PassportPath
+                            userData[16],                        // ResidenceCardPath
+                            Convert.ToDecimal(userData[17]),     // Balance
+                            userData[18],                        // CardNumber
+                            ClientFilePath
+                        );
+                        clientDetailForm.Show();
+                        return; // للخروج من الدالة بعد العثور على الشخص
+                    }
+                }
+            }
         }
 
         private void guna2Button1_Click(object sender, EventArgs e)
